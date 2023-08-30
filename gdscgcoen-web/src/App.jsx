9500f5e7009e8b0e842821d93ad15d0e598be8b0
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-08-30 19:34:05 +0530

    dropdown

--- gdscgcoen-web/src/App.jsx
+++ gdscgcoen-web/src/App.jsx
@@ -1,11 +1,22 @@
 import Header from './Header'
 import './assets/App.css'
+import {useState,useEffect} from 'react'
 function App() {
-
+const [isMobile,setIsMobile]= useState(window.innerWidth <768);
+const handleSetMobile = () => {
+  setIsMobile(window.innerWidth <768);
+}
+useEffect(() => {
+  handleSetMobile()
+  window.addEventListener("resize", handleSetMobile)
+  return () => {
+      window.removeEventListener("resize",handleSetMobile)
+  }
+}, [])
 
   return (
     <>
-    <Header/>
+    <Header mobile={isMobile}/>
     </>
   )
 }
--- gdscgcoen-web/src/Header.jsx
+++ gdscgcoen-web/src/Header.jsx
@@ -1,8 +1,20 @@
 import './assets/App.css'
 import gdscIcon from './assets/images/gdsc-icon.webp'
-const Header = ()=>{
+import dropdownIcon from './assets/images/menu-export.png'
+import {useState,useEffect} from 'react'
+const Header = (props)=>{
+    const [mobile,setMobile]=useState(props.mobile);
+    useEffect(()=>{
+        setMobile(props.mobile);
+    },[props.mobile])
 return(<>
-<div className="header" style={{height:'5rem'}}><img src={gdscIcon} style={{width:'height',height:'3rem'}}></img> <div style={{display:'flex',flexDirection:'column'}}><p style={{margin:'0rem'}}>GDSC</p><p style={{margin:'0rem'}}>GCOEN</p></div> </div>
+<div className="header" style={{height:'5rem',alignItems:'center',justifyContent:mobile?'space-between':'end'}}><img src={dropdownIcon} style={{display:mobile?'block':'none',width:'height',height:'2rem',marginLeft:'1rem'}}></img>
+<div style={{height:'100%',width:'auto',alignContent:'center',display:'flex'}}>
+<img src={gdscIcon} style={{width:'height',height:'1.5rem',marginLeft:'1rem',marginRight:'0.5rem'}}></img> 
+<div style={{width:'auto',marginLeft:'0.5rem'}}>
+<p style={{margin:'0rem',fontSize:'1.2rem',width:'100%',color:'#5F6368',fontWeight:'1%'}}>GDSC GCOEN</p>
+</div>
+</div> </div>
 </>)
 }
 export default Header
